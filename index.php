7b9893613e9792594fbe1435324463ece341c8b0
SbWereWolf <ulfnew@gmail.com>
2019-10-31 06:20:58 -0700

    Add bitrix basis site
     * Add git ignore rules

---
+++ index.php
@@ -0,0 +1,6 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Демонстрационная версия продукта «1С-Битрикс: Управление сайтом»");
+$APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
+$APPLICATION->SetTitle("Главная страница");
+?><h2>Hello world, it is a Scheme!</h2><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
