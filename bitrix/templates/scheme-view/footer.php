6751d0129787efe699a27b1f1dd9417f2ce34b3b
SbWereWolf <ulfnew@gmail.com>
2019-11-18 18:49:18 +0500

    Move bootstrap.js to footer fo `scheme` theme

--- bitrix/templates/scheme-view/footer.php
+++ bitrix/templates/scheme-view/footer.php
@@ -1,1 +1,7 @@
+<footer>
+    <script crossorigin="anonymous"
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    ></script>
+</footer>
 </html>
